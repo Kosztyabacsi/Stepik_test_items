8fa2e41f0b16baedad2891bfa8bf11ae2cc247ef
Konstantin Kashin <konsztantinkasin@gmail.com>
2022-03-18 11:02:52 +0100

    test_items_3_6_9.py - pause reduced to 2 seconds

--- test_items_3_6_9.py
+++ test_items_3_6_9.py
@@ -4,7 +4,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_button_add_to_basket(browser):
     browser.get(link)
-    time.sleep(10)
+    time.sleep(2)
     button = browser.find_elements_by_css_selector(".btn-add-to-basket")
     assert button is not None, "No button Add-to-basket!"
 
